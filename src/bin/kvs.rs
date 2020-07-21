acdba4a59b1f387a99d4d408980fd249244055e0
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-21 13:38:03 -0700

    feat: project 1 part 5

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -3,23 +3,23 @@ extern crate clap;
 use clap::{load_yaml, App};
 use std::process;
 fn main() {
-  let yaml = load_yaml!("cli.yml");
-  let config = App::from(yaml)
-    .version(crate_version!())
-    .author(crate_authors!())
-    .name(crate_name!());
+    let yaml = load_yaml!("cli.yml");
+    let config = App::from(yaml)
+        .version(crate_version!())
+        .author(crate_authors!())
+        .name(crate_name!());
 
-  let matches = config.get_matches();
+    let matches = config.get_matches();
 
-  match matches.subcommand() {
-    ("get", Some(sub_input)) => err_unimplemented(sub_input),
-    ("set", Some(sub_input)) => err_unimplemented(sub_input),
-    ("rm", Some(sub_input)) => err_unimplemented(sub_input),
-    _ => println!("no command matches"),
-  }
+    match matches.subcommand() {
+        ("get", Some(sub_input)) => err_unimplemented(sub_input),
+        ("set", Some(sub_input)) => err_unimplemented(sub_input),
+        ("rm", Some(sub_input)) => err_unimplemented(sub_input),
+        _ => println!("no command matches"),
+    }
 
-  fn err_unimplemented(_: &clap::ArgMatches) {
-    eprintln!("unimplemented");
-    process::exit(1);
-  }
+    fn err_unimplemented(_: &clap::ArgMatches) {
+        eprintln!("unimplemented");
+        process::exit(1);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,57 @@
+#![deny(missing_docs)]
+//! kvs is a command line key value store
 use std::collections::HashMap;
 
+/// KvStore struct definition
+#[derive(Default)]
 pub struct KvStore {
   store: HashMap<String, String>,
 }
 
 impl KvStore {
-  pub fn new() -> KvStore {
+  /// Creates a new KvStore
+  /// # Examples
+  /// ```
+  /// let store = KvStore::new();
+  /// ```
+  pub fn new() -> Self {
     KvStore {
       store: HashMap::new(),
     }
   }
 
-  pub fn get(&self, key: String) -> Option<String> {
-    return self.store.get(&key).cloned();
-  }
-
+  /// Sets a value for a particular key
+  /// # Example
+  ///
+  /// ```
+  /// let store = KvStore::new();
+  /// store.set("key1".to_owned(), "value1".to_owned());
+  /// ```
   pub fn set(&mut self, key: String, value: String) {
     self.store.insert(key, value);
   }
 
+  /// Gets a value for a particular key.
+  /// Returns `None` if a value is not found.
+  /// # Example
+  ///
+  /// ```
+  /// let store = KvStore::new();
+  /// store.set("key1".to_owned(), "value1".to_owned());
+  /// store.get("key1".to_owned());
+  /// ```
+  pub fn get(&mut self, key: String) -> Option<String> {
+    self.store.get(&key).cloned()
+  }
+
+  /// Removes a value at a particular key.
+  /// # Examples
+  ///
+  /// ```
+  /// let store = KvStore::new();
+  /// store.set("key1".to_owned(), "value1".to_owned());
+  /// store.remove("key1".to_owned());
+  /// ```
   pub fn remove(&mut self, key: String) {
     self.store.remove(&key);
   }
