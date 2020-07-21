861321dfbf2e3ed570c79d74861c2499cdd5abd2
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-21 12:14:33 -0700

    feat: project 1 part 1

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub struct KvStore {}
+
+impl KvStore {
+  pub fn new() -> KvStore {
+    KvStore {}
+  }
+
+  pub fn get(&self, _: String) -> Option<String> {
+    return Some("howdy".to_string());
+  }
+
+  pub fn set(&self, _: String, __: String) {}
+
+  pub fn remove(&self, _: String) {}
+}
