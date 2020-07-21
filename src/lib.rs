63e4402b975ef276e38d4dd6e41d6e78f6b2c7b1
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-21 13:32:53 -0700

    feat: project 1 part 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+  store: HashMap<String, String>,
+}
 
 impl KvStore {
-    pub fn new() -> KvStore {
-        KvStore {}
+  pub fn new() -> KvStore {
+    KvStore {
+      store: HashMap::new(),
     }
+  }
 
-    pub fn get(&self, _: String) -> Option<String> {
-        return Some("howdy".to_string());
-    }
+  pub fn get(&self, key: String) -> Option<String> {
+    return self.store.get(&key).cloned();
+  }
 
-    pub fn set(&self, _: String, __: String) {}
+  pub fn set(&mut self, key: String, value: String) {
+    self.store.insert(key, value);
+  }
 
-    pub fn remove(&self, _: String) {}
+  pub fn remove(&mut self, key: String) {
+    self.store.remove(&key);
+  }
 }
