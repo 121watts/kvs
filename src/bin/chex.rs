43ce76ae40e26ff0ff7a9a2fd0e76add0eacc3d1
Andrew Watkins <andrew.watkinz@gmail.com>
2020-07-22 19:23:19 -0700

    feat: building block exercises

---
+++ src/bin/chex.rs
@@ -0,0 +1,56 @@
+extern crate serde;
+use std::fs::File;
+use std::io::BufReader;
+use serde::{Serialize, Deserialize};
+use bincode;
+use ron::ser::{to_string_pretty, PrettyConfig};
+use ron::de::from_str;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Move {
+  up: u8,
+  down: u8,
+}
+
+fn main() -> std::io::Result<()> {
+  let a = Move {
+    up: 1,
+    down: 10,
+  };
+
+  // JSON to and from file
+  let file = File::create("move.json")?;
+
+  let serialized_json = serde_json::to_writer_pretty(file, &a).unwrap();
+
+
+  let file = File::open("move.json")?;
+  let reader = BufReader::new(file);
+  let deserialized_json: Move = serde_json::from_reader(reader).unwrap();
+
+  println!("serialized json = {:?}", serialized_json);
+  println!("deserialized = {:?}", deserialized_json);
+
+
+  let serialized_vec = bincode::serialize(&a).unwrap();
+  let deserialized_vec: Move = bincode::deserialize(&serialized_vec).unwrap();
+
+  println!("serialized Vec<u8> = {:?}", serialized_vec);
+  println!("deserialized Vec<u8> = {:?}", deserialized_vec);
+
+
+  let config = PrettyConfig::new();
+  let sr = to_string_pretty(&a, config).unwrap();
+  let dsr: Move = from_str(&sr).unwrap();
+
+  println!("serialized ron = {:?}", sr);
+  println!("deserialized ron = {:?}", dsr);
+
+  Ok(())
+
+  // let serialized_json = serde_json::to_string(&a).unwrap();
+
+  // let deserialized_json: Move = serde_json::from_str(&serialized_json).unwrap();
+  // println!("deserialized = {:?}", deserialized_json);
+
+}
